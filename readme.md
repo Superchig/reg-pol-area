7126e477d344328d0167a6e64170885781347802
Christopher <chiggiechang@gmail.com>
2015-04-12 20:26:09 -0700

    YUS! It works!

--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
+# Readme
 Something that may just allow me to find the area of regular polygons.
+Using minitest.
+
+## TO-DO
+
+1. Learn markdown.
+2. Learn ruby.
+3. Learn rake.
+4. Use all of the above in this monstrosity.
--- src/PolyMath.rb
+++ src/PolyMath.rb
@@ -1,49 +1,43 @@
 require 'bigdecimal'
 require 'bigdecimal/util'
 
+class Numeric
+	def degrees_to_radians
+		self * Math::PI / 180
+	end
+end
+
 module PolyMath
-	def self.radianToDegree(radian, roundTo=5)
-		radian = radian.to_d
-		radian *= 180
-		radian /= Math.pi
-		radian.round(roundTo)
+
+	def self.calcApothemWithRadius(radius, sides)
+		ang1 = 180 / sides
+		apothem = radius * Math.cos(ang1.degrees_to_radians)
+	end
+
+	def self.calcPerimeterWithRadius(radius, sides)
+		ang1 = 180 / sides
+		twoRadius = radius * 2
+		sinAng1 = Math.sin(ang1.degrees_to_radians)
+		sideLength = twoRadius * sinAng1
+		perimeter = sides * sideLength
 	end
 
 	def self.findAreaWithRadius(radius, sides)
-		def self.calcApothem(radius, sides)
-			step1 = Math.cos(180 / sides)
-			puts "step1 (radians) is: #{step1}"
-			step1 = radianToDegree(step1, 9)
-			puts "step1 is: #{step1}"
-			step1 = step1.to_d
-			puts "step1 is: #{step1}"
-			apothem = step1 * radius
-		end
 
-		apothem = calcApothem(radius, sides)
+		apothem = calcApothemWithRadius(radius, sides).to_d
 		puts "apothem is: #{apothem}"
 
-		def self.calcPerimeter(radius, sides)
-			twoRadius = radius * 2
-			sides180 = 180 / sides
-			puts "sides180 is: #{sides180}"
-			sinSides180Radian = Math.sin(sides180)
-			sinSides180 = radianToDegree(sinSides180Radian)
-			puts "sinSides180 is: #{sinSides180}"
-			sideLength = twoRadius * sinSides180 
-			puts "sideLength is: #{sideLength}"
+		apothemIsBigDec = apothem.is_a? BigDecimal
+		puts apothemIsBigDec ? "apothem is a BigDecimal" : "apothem is not a BigDecimal"
 
-			perimeter = sideLength * sides
-		end
-
-		perimeter = calcPerimeter(radius, sides)
+		perimeter = calcPerimeterWithRadius(radius, sides).to_d
 		puts "perimeter is: #{perimeter}"
 
 		perimeterIsBigDec = perimeter.is_a? BigDecimal
 		puts perimeterIsBigDec ? "perimeter is a BigDecimal" : "perimeter is not a BigDecimal"
 
 		area = (apothem * perimeter) / 2
-		puts "area is: #{perimeter}"
+		puts "area is: #{area}"
 
 		areaIsBigDec = area.is_a? BigDecimal
 		puts areaIsBigDec ? "area is a BigDecimal" : "area is not a BigDecimal"
