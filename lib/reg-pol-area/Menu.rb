3bd9601f7d9e1274695cb0da756819b9c49a9fdc
Christopher <chiggiechang@gmail.com>
2015-04-16 20:18:34 -0700

    Mostly finished the menu, may add some finishing touches yet.

--- lib/reg-pol-area/Menu.rb
+++ lib/reg-pol-area/Menu.rb
@@ -3,62 +3,78 @@
 require_relative 'PolyMath'
 
 module Menu
-	def self.show
-		puts "Welcome to an area-of-regular-polygon calculator. What values do you know?\n"
+  def self.show
+	puts "Welcome to an area-of-regular-polygon calculator. What values do you know?\n"
 
-		puts "1. Radius and number of sides."
-		puts "2. Apothem and number of sides."
-		puts "3. Exit"
+	puts "1. Radius and number of sides."
+	puts "2. Apothem and number of sides."
+	puts "3. Side length and number of sides."
+	puts "4. Exit"
 
-		puts "I realize that exit is not a value that belongs to a regular polygon.\n"
+	puts "I realize that exit is not a value that belongs to a regular polygon.\n"
 
-		puts ""
+	puts ""
 
-		print "Input choice> "
+	print "Input choice> "
+  end
+
+  def self.evalChoice(choice)
+	unicode_squared = "\U+00B2"
+	unicode_squared = unicode_squared.encode('utf-8')
+	case choice
+	when '1'
+	  print "Please input the radius> "
+	  radius = gets.chomp.to_i
+
+	  puts ""
+
+	  print "Please input the number of sides> "
+	  sides = gets.chomp.to_i
+
+	  puts ""
+
+	  answer = PolyMath.findAreaWithRadius(radius, sides).to_f
+	  answer = answer.round(2)
+
+	  puts "\nThis regular polygon is approximately #{answer} units^2"
+	  true
+	when '2'
+	  print "Please input the apothem> "
+	  apothem = gets.chomp.to_i
+
+	  puts ""
+
+	  print "Please input the number of sides> "
+	  sides = gets.chomp.to_i
+
+	  puts ""
+
+	  answer = PolyMath.findAreaWithApothem(apothem, sides).to_f
+	  answer = answer.round(2)
+
+	  puts "\nThis regular polygon is approximately #{answer} units^2"
+	  true
+	when '3'
+	  print "Please input the Side Length> "
+	  sideLength = gets.chomp.to_i
+
+	  puts ""
+
+	  print "Please input the number of sides> "
+	  sides = gets.chomp.to_i
+
+	  puts ""
+
+	  answer = PolyMath.findAreaWithSide(sideLength, sides).to_f
+	  answer = answer.round(2)
+
+	  puts "\nThis regular polygon is approximately #{answer} units^2"
+	when '4'
+	  puts "\nExiting..."
+	  false
+	else
+	  puts "It appears that you have not chosen a valid, well, choice. You're going to have to try again."
+	  true
 	end
-
-	def self.evalChoice(choice)
-		unicode_squared = "\U+00B2"
-		unicode_squared = unicode_squared.encode('utf-8')
-		case choice
-		when '1'
-			print "Please input the radius> "
-			radius = gets.chomp.to_i
-
-			puts ""
-
-			print "Please input the number of sides> "
-			sides = gets.chomp.to_i
-
-			puts ""
-
-			answer = PolyMath.findAreaWithRadius(radius, sides).to_f
-			answer = answer.round(2)
-
-			puts "\nThis regular polygon is approximately #{answer} units^2"
-			true
-		when '2'
-			print "Please input the apothem> "
-			apothem = gets.chomp.to_i
-
-			puts ""
-
-			print "Please input the number of sides> "
-			sides= gets.chomp.to_i
-
-			puts ""
-
-			answer = PolyMath.findAreaWithApothem(apothem, sides).to_f
-			answer = answer.round(2)
-
-			puts "\nThis regular polygon is approximately #{answer} units^2"
-			true
-		when '3'
-			puts "\nExiting..."
-			false
-		else
-			puts "It appears that you have not chosen a valid, well, choice. You're going to have to try again."
-			true
-		end
-	end
+  end
 end
--- lib/reg-pol-area/PolyMath.rb
+++ lib/reg-pol-area/PolyMath.rb
@@ -2,53 +2,53 @@ require 'bigdecimal'
 require 'bigdecimal/util'
 
 class Numeric
-	def degrees_to_radians
-		self * Math::PI / 180
-	end
+  def degrees_to_radians
+	self * Math::PI / 180
+  end
 end
 
 module PolyMath
 
-	def self.calcApothemWithRadius(radius, sides)
-		ang1 = 180.to_d / sides
-		puts "ang1 is: #{ang1}"
-		apothem = radius * Math.cos(ang1.degrees_to_radians)
-	end
+  def self.calcApothemWithRadius(radius, sides)
+	ang1 = 180.to_d / sides
+	puts "ang1 is: #{ang1}"
+	apothem = radius * Math.cos(ang1.degrees_to_radians)
+  end
 
-	def self.calcPerimeterWithRadius(radius, sides)
-		ang1 = 180.to_d / sides
-		twoRadius = radius * 2
-		sinAng1 = Math.sin(ang1.degrees_to_radians)
-		sideLength = twoRadius * sinAng1
-		perimeter = sides * sideLength
-	end
+  def self.calcPerimeterWithRadius(radius, sides)
+	ang1 = 180.to_d / sides
+	twoRadius = radius * 2
+	sinAng1 = Math.sin(ang1.degrees_to_radians)
+	sideLength = twoRadius * sinAng1
+	perimeter = sides * sideLength
+  end
 
-	def self.calcPerimeterWithApothem(apothem, sides)
-		ang1 = 180.to_d / sides
-		tanAng1 = Math.tan(ang1.degrees_to_radians)
-		sideLength = 2 * apothem * tanAng1
-		perimeter = sideLength * sides
-		perimeter = perimeter.to_d
-	end
+  def self.calcPerimeterWithApothem(apothem, sides)
+	ang1 = 180.to_d / sides
+	tanAng1 = Math.tan(ang1.degrees_to_radians)
+	sideLength = 2 * apothem * tanAng1
+	perimeter = sideLength * sides
+	perimeter = perimeter.to_d
+  end
 
-	def self.findAreaWithRadius(radius, sides)
-		apothem = calcApothemWithRadius(radius, sides).to_d
-		perimeter = calcPerimeterWithRadius(radius, sides).to_d
-		area = (apothem * perimeter) / 2
-	end
+  def self.findAreaWithRadius(radius, sides)
+	apothem = calcApothemWithRadius(radius, sides).to_d
+	perimeter = calcPerimeterWithRadius(radius, sides).to_d
+	area = (apothem * perimeter) / 2
+  end
 
-	def self.findAreaWithApothem(apothem, sides)
-		perimeter = calcPerimeterWithApothem(apothem, sides).to_d
-		area = (apothem * perimeter) / 2
-	end
+  def self.findAreaWithApothem(apothem, sides)
+	perimeter = calcPerimeterWithApothem(apothem, sides).to_d
+	area = (apothem * perimeter) / 2
+  end
 
-	def self.findAreaWithSide(sideLength, sides)
-		dividend = sideLength**2 * sides
-		puts "dividend is: #{dividend}"
-		ang1 = 180.to_d / sides
-		divisor = 4 * Math.tan((ang1).degrees_to_radians)
-		puts "divisor is: #{divisor}"
-		area = dividend / divisor
-		area.to_d
-	end
+  def self.findAreaWithSide(sideLength, sides)
+	dividend = sideLength**2 * sides
+	puts "dividend is: #{dividend}"
+	ang1 = 180.to_d / sides
+	divisor = 4 * Math.tan((ang1).degrees_to_radians)
+	puts "divisor is: #{divisor}"
+	area = dividend / divisor
+	area.to_d
+  end
 end
